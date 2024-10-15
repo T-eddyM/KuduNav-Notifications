9c3efa34e58ad8008c7673a1899de380475c4799
Teddy <mteddy941@gmail.com>
2024-10-15 10:21:18 +0200

    save

--- src/push notification/firebase.js
+++ src/push notification/firebase.js
@@ -1,27 +1,13 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
+import dotev from "dotenv";
 
+dotev.config();
 
-const filename = 'kudunot-firebase-adminsdk-csi12-2bbc9e96d2.json';
-
-const dirname = path.resolve();
-
-let serviceAccountPath = path.join(dirname, 'src', 'constants', filename);
-
-/* const pathSegments = serviceAccountPath.split(path.sep);
-
-const occurrences = pathSegments.filter(segment => segment === 'KuduNav-Notifications').length;
-
-if (occurrences > 1) {
-    // Remove one instance of 'KuduNav-Notifications'
-    serviceAccountPath = serviceAccountPath.replace(/(KuduNav-Notifications)(?=.*KuduNav-Notifications)/, '');
-}
-
-// Trim any potential leading/trailing slashes after modification
-serviceAccountPath = serviceAccountPath.replace(/\/{2,}/g, '/');
- */
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+};
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
