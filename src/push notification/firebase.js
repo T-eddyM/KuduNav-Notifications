aa6b0e2f8aa88595cd9f1b4e7d0c53928e57df3f
Teddy <mteddy941@gmail.com>
2024-10-15 09:53:49 +0200

    save

--- src/push notification/firebase.js
+++ src/push notification/firebase.js
@@ -2,10 +2,9 @@ import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
 
-// Define the filename and dirname
-const filename = '/src/constants/kudunot-firebase-adminsdk-csi12-2bbc9e96d2.json';
-const dirname = path.resolve(); // Gets the current working directory
-const __filename = path.join(dirname, filename);
+const filename = 'kudunot-firebase-adminsdk-csi12-2bbc9e96d2.json';
+const dirname = path.resolve();
+const __filename = path.join(dirname, 'src/constants', filename);
 
 const serviceAccount = JSON.parse(fs.readFileSync(__filename, 'utf-8'));
 
