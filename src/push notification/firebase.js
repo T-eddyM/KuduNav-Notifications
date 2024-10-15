19d5ba953bef0ba04b6a4045f2590a6fd3d4218a
Teddy <mteddy941@gmail.com>
2024-10-15 10:01:13 +0200

    save

--- src/push notification/firebase.js
+++ src/push notification/firebase.js
@@ -9,6 +9,12 @@ const dirname = path.resolve();
 
 const serviceAccountPath = path.join(dirname, 'src', 'constants', filename);
 
+// Check if 'KuduNav-Notifications' is in the path more than once
+if ((serviceAccountPath.match(/KuduNav-Notifications/g) || []).length > 1) {
+    // Remove one instance of 'KuduNav-Notifications'
+    serviceAccountPath = serviceAccountPath.replace(/(.*)(KuduNav-Notifications)(.*)\1/, '$1$2$3');
+}
+
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 // Initialize the Firebase Admin SDK
