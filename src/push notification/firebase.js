91193bb77419cc3f271c21d4c8721f667f2bd48d
Teddy <mteddy941@gmail.com>
2024-10-15 09:58:32 +0200

    save

--- src/push notification/firebase.js
+++ src/push notification/firebase.js
@@ -2,11 +2,14 @@ import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
 
-const filename = 'kudunot-firebase-adminsdk-csi12-2bbc9e96d2.json';
-const dirname = path.resolve();
-const __filename = path.join(dirname, 'src/constants', filename);
 
-const serviceAccount = JSON.parse(fs.readFileSync(__filename, 'utf-8'));
+const filename = 'kudunot-firebase-adminsdk-csi12-2bbc9e96d2.json';
+
+const dirname = path.resolve();
+
+const serviceAccountPath = path.join(dirname, 'src', 'constants', filename);
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
