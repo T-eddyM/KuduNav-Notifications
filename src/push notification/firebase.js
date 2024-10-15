91b65a75bd080b6cb933e55e065f45332a91f418
Teddy <mteddy941@gmail.com>
2024-10-15 10:09:42 +0200

    save

--- src/push notification/firebase.js
+++ src/push notification/firebase.js
@@ -9,16 +9,17 @@ const dirname = path.resolve();
 
 let serviceAccountPath = path.join(dirname, 'src', 'constants', filename);
 
-// Split the path into segments
 const pathSegments = serviceAccountPath.split(path.sep);
 
-// Count occurrences of 'KuduNav-Notifications'
 const occurrences = pathSegments.filter(segment => segment === 'KuduNav-Notifications').length;
 
 if (occurrences > 1) {
     // Remove one instance of 'KuduNav-Notifications'
     serviceAccountPath = serviceAccountPath.replace(/(KuduNav-Notifications)(?=.*KuduNav-Notifications)/, '');
 }
+
+// Trim any potential leading/trailing slashes after modification
+serviceAccountPath = serviceAccountPath.replace(/\/{2,}/g, '/');
 
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
